406849110730ffafd5c69530f05354b4e625cb8b
Kadir Yazadzhi <smx590751@gmail.com>
2025-01-24 02:08:40 +0200

    Create 01. Action Print.cs

---
+++ Exercise - Functional Programming/01. Action Print.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program {
+    public static void Main() {
+        Action<string> Print = name => Console.WriteLine(name);
+        
+        string[] names = Console.ReadLine().Split().ToArray(); 
+        
+        names.ToList().ForEach(Print);
+    }
+}
