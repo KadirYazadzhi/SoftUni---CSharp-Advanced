d0b9effd304d95d1bed26dfc167a973093ed07d8
Kadir Yazadzhi <smx590751@gmail.com>
2025-02-02 20:39:31 +0200

    Add Array Creator Exercise

---
+++ Generics/02. Array Creator/Code/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace GenericArrayCreator {
+    class StartUp {
+        public static void Main(string[] args) {
+            
+        }
+    }
+}
