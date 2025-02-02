d087456b55455d8793a0af8c34ee95dc01106c25
Kadir Yazadzhi <smx590751@gmail.com>
2025-02-02 20:39:05 +0200

    Add Box Exercise

---
+++ Generics/01. Box/Code/Program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BoxOfT {
+    class StartUp {
+        public static void Main(string[] args) {
+            
+        }
+    }
+}
+
---
+++ Generics/02. Array Creator/Code/GenericArrayCreator.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace GenericArrayCreator {
+  public class ArrayCreator {
+        public static T[] Create<T> (int length, T item) {
+            T[] items = new T[length];
+
+            for (int i = 0; i < length; i++) {
+                items[i] = item;
+            }
+            
+            return items;
+        }
+    }
+}
