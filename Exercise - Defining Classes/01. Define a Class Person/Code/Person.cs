2341a5bd13edb1e4618dcaaff12108203b515646
Kadir Yazadzhi <smx590751@gmail.com>
2025-02-09 21:59:02 +0200

    Add Define a class person exercise

--- Exercise - Defining Classes/01. Define a Class Person/Code/Person.cs
+++
@@ -1,9 +0,0 @@
-namespace  DefiningClasses {
-    public class Person {
-        private string name;
-        private int age;
-        
-        public string Name { get => name; set => name = value; }
-        public int Age { get => age; set => age = value; }
-    }
-}
