65e5a723ec13ce8fdfe9c26eb1cc973de8d02b68
Kadir Yazadzhi <smx590751@gmail.com>
2025-02-10 18:19:19 +0200

    Add Generic Count Method Double Exercise

---
+++ Exercise - Generics/06. Generic Count Method Double/Code/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBox {
+    public class StartUp {
+        static void Main() {
+            double n = double.Parse(Console.ReadLine());
+            Box<double> box = new Box<double>();
+
+            for (int i = 0; i < n; i++) {
+                double input = double.Parse(Console.ReadLine());
+                box.Add(input);
+            }
+            
+            double element = double.Parse(Console.ReadLine());
+            Console.WriteLine(box.CountGreaterThan(element));
+        }
+    }
+}
