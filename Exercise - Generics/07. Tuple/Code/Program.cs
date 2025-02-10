e0aaf5dea3201da26353fdf1c0df8e8a7b3197ce
Kadir Yazadzhi <smx590751@gmail.com>
2025-02-10 18:19:35 +0200

    Add Tuple Exercise

---
+++ Exercise - Generics/07. Tuple/Code/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuple {
+    public class StartUp {
+        static void Main() {
+            string[] firstInput = Console.ReadLine().Split(' ', 3);
+            var tuple1 = new Tuple<string, string>($"{firstInput[0]} {firstInput[1]}", firstInput[2]);
+        
+            string[] secondInput = Console.ReadLine().Split();
+            var tuple2 = new Tuple<string, int>(secondInput[0], int.Parse(secondInput[1]));
+        
+            string[] thirdInput = Console.ReadLine().Split();
+            var tuple3 = new Tuple<int, double>(int.Parse(thirdInput[0]), double.Parse(thirdInput[1]));
+        
+            Console.WriteLine(tuple1);
+            Console.WriteLine(tuple2);
+            Console.WriteLine(tuple3);
+        }
+    }
+}
