e26bf9a0ca5e053bc1e78c6febe374e9accd1a38
Kadir Yazadzhi <smx590751@gmail.com>
2025-02-11 18:20:16 +0200

    Add Threeuple Exercise

---
+++ Exercise - Generics/08. Threeuple/Code/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threeuple {
+    public class StartUp {
+        static void Main() {
+            string[] firstInput = Console.ReadLine().Split(' ', 4);
+            var threeuple1 = new Threeuple<string, string, string>($"{firstInput[0]} {firstInput[1]}", firstInput[2], firstInput[3]);
+        
+            string[] secondInput = Console.ReadLine().Split();
+            var threeuple2 = new Threeuple<string, int, bool>(secondInput[0], int.Parse(secondInput[1]), secondInput[2].ToLower() == "drunk");
+        
+            string[] thirdInput = Console.ReadLine().Split();
+            var threeuple3 = new Threeuple<string, double, string>(thirdInput[0], double.Parse(thirdInput[1]), thirdInput[2]);
+        
+            Console.WriteLine(threeuple1);
+            Console.WriteLine(threeuple2);
+            Console.WriteLine(threeuple3);
+        }
+    }
+}
