7afe9435072ff42766304faac02839f2c9a46945
Kadir Yazadzhi <smx590751@gmail.com>
2025-02-11 18:22:15 +0200

    Add Book Comparer Exercise

---
+++ Iterators and Comparators/04. BookComparer/Code/Book.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators {
+    public class Book : IComparable<Book> {
+        public string Title { get; }
+        public int Year { get; }
+        public List<string> Authors { get; }
+
+        public Book(string title, int year, params string[] authors) {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = new List<string>(authors);
+        }
+
+        public int CompareTo(Book? other) {
+            if (other is null) return -1;
+
+            int yearComparison = this.Year.CompareTo(other.Year);
+            if (yearComparison != 0) return yearComparison;
+
+            return this.Title.CompareTo(other.Title);
+        }
+
+        public override string ToString() => $"{this.Title} - {this.Year}";
+    }  
+}
