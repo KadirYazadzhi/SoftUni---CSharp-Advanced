6eb8e66f3e6383cc35fadc900fb114cc91d37f41
Kadir Yazadzhi <smx590751@gmail.com>
2025-02-11 18:20:53 +0200

    Add Library Exercise

---
+++ Iterators and Comparators/01. Library/Code/Library.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators {
+    public class Library : IEnumerable<Book> {
+        private List<Book> books;
+
+        public Library(params Book[] books) {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator() {
+            return books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+    }  
+}
