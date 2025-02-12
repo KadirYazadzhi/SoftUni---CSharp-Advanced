30aba10d5e3a44b625f7005076b769661fc33e1a
Kadir Yazadzhi <smx590751@gmail.com>
2025-02-12 17:59:22 +0200

    Add Collection Exercise

---
+++ Exercise - Iterators and Comparators/02. Collection/Code/ListyIterator.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+
+namespace ListyIterator;
+
+public class ListyIterator<T> : IEnumerable<T> {
+    private readonly List<T> list;
+    private int index;
+
+    public ListyIterator(IEnumerable<T> values) {
+        this.list = new List<T>(values);
+    }
+
+    public T Current => this.GetCurrentElement();
+
+    public bool Move() {
+        if (!this.HasNext()) return false;
+        
+        this.index++;
+        return true;
+    }
+
+    public bool HasNext() => this.index + 1 < this.list.Count;
+
+    private T GetCurrentElement() {
+        if (this.index >= this.list.Count) throw new InvalidOperationException("Invalid Operation!");
+        
+        return this.list[this.index];
+    }
+
+    public IEnumerator<T> GetEnumerator() {
+        for (int i = 0; i < this.list.Count; i++) {
+            yield return this.list[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+}
